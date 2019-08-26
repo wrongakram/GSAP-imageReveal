dd2bd29bbe857acbf93af6feece8f1011415860e
akram <akramkhalid12@gmail.com>
2019-08-25 19:37:47 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef, useEffect} from 'react';
 import './App.css';
+import Mountain from './images/mountains.jpg'
+import {TweenLite, Power3} from 'gsap';
+import CSSRulePlugin from "gsap/CSSRulePlugin";
 
-function App() {
+
+
+const App = () => {
+  let image = useRef(null)
+  let imageContainer = useRef(null)
+  let imageReveal = CSSRulePlugin.getRule(".img-container:after");
+
+  useEffect(() => {
+    TweenLite.from(imageReveal, 1, {width: "700px", ease: Power3.easeOut})
+    TweenLite.from(image, 1, {scale: 1.3, delay: .3, ease: Power3.easeOut})
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="wrapper">
+        <div ref={el => {imageContainer = el}} className="img-container">
+          <img ref={el => {image = el} } src={Mountain} alt="idk" />
+        </div>
+     </div>
     </div>
   );
 }
